8edfea25bb71d27c1b52920a5db80a6e8b1efe1b
CoreVanta <eng.freethinker@gmail.com>
2025-12-08 06:05:21 +0200

    Delete js directory

--- js/firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyB3nTJlyM3K-XpvEUrXyk3leiVeMxzkTiU",
-  authDomain: "forexvolcano-21180.firebaseapp.com",
-  projectId: "forexvolcano-21180",
-  storageBucket: "forexvolcano-21180.firebasestorage.app",
-  messagingSenderId: "264687169302",
-  appId: "1:264687169302:web:83322d7adc303b1f0c2a37",
-  measurementId: "G-XTB0D1TPS6"
-};
